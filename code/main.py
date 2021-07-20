0797a1d4c39619705bf939922b10886ab83dfcd6
vbkantoine <71443616+vbkantoine@users.noreply.github.com>
2021-07-20 15:24:44 +0200

    Add files via upload

--- code/main.py
+++ code/main.py
@@ -37,15 +37,15 @@ S_tot, A_tot = get_S_A(path, IM, C, shuffle=False, relative=False)
 num_tot = A_tot.shape[0]
 
 num_est = 50
-S_ref, A_ref = S_tot[:num_tot], A_tot[:num_tot]
+S_ref, A_ref = S_tot[:num_est], A_tot[:num_est]
 
 
 # tirage de nombreux MLE
 
 def func_log_vr(z, a) :
     def log_vr(theta) :
-        # return -stat_functions.log_vrais(z, a, theta.reshape(1,2))
-        return ref.opp_log_vraiss(theta, a, z)
+        return -stat_functions.log_vrais(z, a, theta.reshape(1,2))
+        # return ref.opp_log_vraiss(theta, a, z)
     return log_vr
 
 kmax = int(num_tot/num_est)
@@ -68,10 +68,11 @@ def func_log_post(z,a) :
 
 import time
 log_post = func_log_post(S_ref, A_ref)
-sig_prop = np.array([[0.25,0],[0,0.02]])
+sig_prop = np.sqrt(0.9*np.array([[0.5,0],[0,0.2]]))
 a1 = time.time()
-th_post, accept = stat_functions.HM_k(t0, log_post, kmax, pi_log=True, max_iter=20, sigma_prop=sig_prop) #long time to execute ; prefer opening files from data bellow
-a2 = time.time()
+th_post, accept = stat_functions.HM_k(t0, log_post, kmax, pi_log=True, max_iter=2000, sigma_prop=sig_prop) #long time to execute ; prefer opening files from data bellow
+a2 = time.time()  # time for max_iter=kmax=2000 arround 16.5 minutes
+accept = np.minimum(accept, np.ones_like(accept))
 
 plt.figure()
 plt.plot(th_MLE[:,0], th_MLE[:,1], 'x', label='MLE')
@@ -79,12 +80,12 @@ plt.plot(th_post[:,0], th_post[:,1], 'o', label='Post')
 plt.title(r'Tirages de {} MLE / obs. a posteriori'.format(kmax))
 plt.legend()
 
-# file = open(path+r"/th_post", mode='wb')   # To save the results of the Metropolis-Hasting execution
-# pickle.dump(th_post, file)
-# file.close()
-# file = open(path+r"/accept_ratio", mode='wb')
-# pickle.dump(accept, file)
-# file.close()
+file = open(path+r"/th_post", mode='wb')   # To save the results of the Metropolis-Hasting execution
+pickle.dump(th_post, file)
+file.close()
+file = open(path+r"/accept_ratio", mode='wb')
+pickle.dump(accept, file)
+file.close()
 #
 # file = open(path+r'/th_post', 'rb')   # To load the last results of the Metropolis-Hasting execution instead of executing it again
 # th_post = pickle.load(file)           # instead of executing it again
@@ -118,17 +119,18 @@ curve_med_post = np.median(curves_post, axis=0)
 curve_med_MLE = np.median(curves_MLE, axis=0)
 
 
-fig = plt.figure(figsize=(18,6))
+
+fig = plt.figure(figsize=(13,5))
 
 ax1 = fig.add_subplot(1,2,1)
 ax1.plot(ref.a_tab, curve_med_MLE, '-k', label=r'Median')
 ax1.plot(ref.a_tab, curve_q1_MLE, '--r', label=r'conf {}%'.format(int((1-conf)*100)))
 ax1.plot(ref.a_tab, curve_q2_MLE, '--r')
-ax1.plot(A_ref, S_ref, 'og', markersize = 0.5, alpha=0.6)
+ax1.plot(A_ref, S_ref, 'og', alpha=0.6, fillstyle='none')
 ax1.plot(ref.a_tab, ref.curve_ML, color='magenta', label=r'ref', alpha=0.7)
 ax1.set_title(r'MLE estimation')
 ax1.set_ylabel(r'$P_f(a)$')
-ax1.set_xlabel(r'a='+IM)
+ax1.set_xlabel(r'$a$='+IM)
 ax1.legend()
 
 
@@ -138,7 +140,7 @@ ax2.plot(ref.a_tab, curve_med_post, '-k', label=r'Median', linewidth=0.8)
 ax2.plot(ref.a_tab, ref.curve_ML, color='magenta', label=r'ref', alpha=0.7, linewidth=0.8)
 ax2.plot(ref.a_tab, curve_q1_post, '--r', label=r'conf {}%'.format(int((1-conf)*100)), linewidth=0.8)
 ax2.plot(ref.a_tab, curve_q2_post, '--r', linewidth=0.8)
-ax2.plot(A_ref, S_ref, 'og', markersize=0.5, alpha=0.6)
+ax2.plot(A_ref, S_ref, 'og', alpha=0.6, fillstyle='none')
 
 ax2.set_frame_on(False)
 (xmin, xmax) = ax2.xaxis.get_view_interval()
@@ -148,7 +150,7 @@ ax2.add_artist(plt.Line2D((xmin, xmin), (ymin, ymax),
 ax2.add_artist(plt.Line2D((xmin, xmax), (ymin, ymin),
                               color = 'black', linewidth = 1.5))
 ax2.set_ylabel(r'$P_f(a)$')
-ax2.set_xlabel(r'a='+IM)
+ax2.set_xlabel(r'$a$='+IM)
 ax2.legend()
 
 
@@ -161,6 +163,7 @@ ax22.xaxis.set_label_position('top')
 ax22.yaxis.set_label_position('right')
 
 ax22.plot(accept.mean(axis=1), '--b', label=r'accpet ratio', linewidth=0.5, alpha=0.3)
+# ax22.plot(accept[:,0], '--g', label=r'accpet ratio', linewidth=0.5, alpha=0.3)
 
 ax22.set_frame_on(False)
 ax22.yaxis.tick_right()
@@ -178,7 +181,7 @@ ax22.add_artist(plt.Line2D((xmax, xmax), (ymin, ymax),
                               color = 'blue', linewidth = 1.5))
 ax22.add_artist(plt.Line2D((xmin, xmax), (ymax, ymax),
                               color = 'blue', linewidth = 1.5))
-ax22.set_ylabel(r'accept ratio', color='blue')
+ax22.set_ylabel(r'avg. accept ratio', color='blue')
 ax22.set_xlabel(r'iterations', color='blue')
 ax22.legend()
 
